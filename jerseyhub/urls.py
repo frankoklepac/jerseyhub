f07f0495c7e71c30166fb6ca20c35c64ea3b5182
frankoklepac <itsmefranko1@gmail.com>
2026-01-17 11:01:52 +0100

    Basic user auth was implemented

--- jerseyhub/urls.py
+++ jerseyhub/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('accounts.urls')),
+    path('', views.home, name='home'),
 ]
